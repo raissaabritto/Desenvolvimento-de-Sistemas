960f6586f614570bf707e94231565ae4457e6362
Raíssa Abate <81603586+raissaabritto@users.noreply.github.com>
2023-08-16 20:52:53 -0300

    Atividade Calculadora

--- aula3.1/index.php
+++ aula3.1/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calculadora</title>
 </head>
 <body>
     <!-- Estrutura de metodo get/post que liga com resultado.php (leva pra outra pag) 
@@ -12,12 +12,15 @@
 <form method = "post" action="resultado.php">
 
 Valor 1: 
-<input type="text" name="v1">
+<input type="text" name="valor1">
 <br>
 Valor 2: 
-<input type="text" name="v2">
-<input type="submit" name="Salvar">
-<input type="reset" name="Limpar">
+<input type="text" name="valor2">
+<br>
+<input type="submit" name="operacao" value="soma">
+<input type="submit" name="operacao" value="subtracao">
+<input type="submit" name="operacao" value="multiplicacao">
+<input type="submit" name="operacao" value="divisao">
 
 </form>
 
--- aula3.1/resultado.php
+++ aula3.1/resultado.php
@@ -1,7 +1,32 @@
-<?php
+<!--
     //função get ou post para pegar nome no atributo name - ligando php com html
     //função php para usar tanto get como post $_REQUEST
     $nome = $_REQUEST['nome'];
     echo "Meu nome é: " . $nome;
+-->
 
+<?php
+$valor1 = $_REQUEST['valor1'];
+$valor2 = $_REQUEST['valor2'];
+$operacao = $_REQUEST['operacao'];
+
+switch($operacao){
+    case "soma":
+        $resultado = $valor1 + $valor2;
+        break;
+    case "subtracao":
+        $resultado = $valor1 - $valor2;
+        break;
+    case "multiplicacao":
+        $resultado = $valor1 * $valor2;
+        break;
+    case "divisao":
+        $resultado = $valor1 / $valor2;
+        break;
+
+}
+
+
+echo "Resultado: " .$resultado;
 ?>
+
